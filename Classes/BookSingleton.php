0a4980323a28982b1c8b1e1c8fab72714c4bf88b
meysam <mpiri44@gmail.com>
2018-01-28 14:39:49 +0330

    add new files

---
+++ Classes/BookSingleton.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 1/27/18
+ * Time: 4:16 PM
+ */
+
+class BookSingleton
+{
+
+}
---
+++ views/welcome.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 1/28/18
+ * Time: 2:39 PM
+ */
